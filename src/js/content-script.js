d2a6be8b6eab2b1a5cd9d8ad73a9198d87be8429
JohnnyJianHY@gmail.com <JohnnyJianHY@gmail.com@2e18581b-e063-0c0c-f8ff-ff1efde77aaf>
2012-02-21 02:33:16 +0000

    Show warning for removing limitation of the length of key

--- src/js/content-script.js
+++ src/js/content-script.js
@@ -1,13 +1,27 @@
+function getDefaultKey() {
+    var value = $.getDomain();
+    if (options.isAppendScramble()) {
+        value += options.getScramble();
+    }
+    return value;
+}
+
 function fillKey(reset) {
     $("#flower-password-key").removeClass('flower-password-last flower-password-default');
+    $("#flower-password-no-maxlength").hide();
     if (options.hasLastKey()) {
         var value = options.getLastKey();
         $("#flower-password-key").valLimited(value).change().addClass('flower-password-last');
-    } else if (options.isFillKeyWithDomain()) {
-        var value = $.getDomain();
-        if (options.isAppendScramble()) {
-            value += options.getScramble();
+
+        if (options.isFillKeyWithDomain()) {
+            var defaultKey = getDefaultKey();
+            if (value.length == 15 && defaultKey.length > 15 && defaultKey.indexOf(value) == 0) {
+                $("#flower-password-no-maxlength").show();
+            }
         }
+
+    } else if (options.isFillKeyWithDomain()) {
+        var value = getDefaultKey();
         $("#flower-password-key").valLimited(value).change().addClass('flower-password-default');
     } else if (reset) {
         $("#flower-password-key").val('');
@@ -144,6 +158,12 @@ function lazyInject() {
             '<h1><span id="flower-password-title">花密 Flower Password</span> <a href="http://flowerpassword.com/" target="_blank"><img src="' + getURL('img/goto.png') + '" title="打开花密官网" /></a></h1>' +
             '<div class="flower-password-field"><label for="flower-password-password">记忆密码</label><input id="flower-password-password" name="flower-password-password" type="password" value="" accesskey="S" /></div>' +
             '<div class="flower-password-field"><label for="flower-password-key">区分代号</label><input id="flower-password-key" name="flower-password-key" type="text" value="" /></div>' +
+            '<div id="flower-password-no-maxlength" style="display: none;"><span id="flower-password-no-maxlength-arrow"></span><div id="flower-password-no-maxlength-hint">' +
+                '<p>　旧版花密限制了区分代号的最大长度为15，新版花密已去除此限制，这意味着你可以使用更长更安全的区分代号。</p>' +
+                '<p>　然而，此区分代号是由旧版花密生成的默认区分代号，其长度被限制为15，而由新版生成的默认区分代号则会更长。</p>' +
+                '<p>　一旦您清除了历史记录，新的默认区分代号将导致最终生成的密码发生变化，这将导致您无法登录当前网站！</p>' +
+                '<p>　因此，我们强烈建议您使用新的默认区分代号修改您在当前网站的密码。请您在输入新密码的时候，<a id="flower-password-fill-default-key">点击这里生成新的默认区分代号</a>。</p>' +
+            '</div></div>' +
             '<div class="flower-password-field"><input id="flower-password-fill-key" name="flower-password-fill-key" type="checkbox" /><label for="flower-password-fill-key">默认将网站域名填入区分代号</label></div>' +
             '<div class="flower-password-field"><input id="flower-password-append-scramble" name="flower-password-append-scramble" type="checkbox" /><label for="flower-password-append-scramble">在默认区分代号后加上附加扰码</label></div>' +
             '<div class="flower-password-field" id="flower-password-scramble-field" style="display: none;"><label for="flower-password-scramble">附加扰码</label><input id="flower-password-scramble" name="flower-password-scramble" type="text" value="" /></div>' +
@@ -200,8 +220,13 @@ function lazyInject() {
         }
         if (oldKey != value) {
             e.removeClass('flower-password-last flower-password-default');
+            $("#flower-password-no-maxlength").hide();
             oldKey = value;
         }
+    });
+
+    $('#flower-password-fill-default-key').click(function() {
+        fillDefaultKey();
     });
 
     $('#flower-password-fill-key').prop("checked", options.isFillKeyWithDomain()).change(function() {
